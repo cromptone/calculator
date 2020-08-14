7d1133015621d176711dff8e9c5a94e1965cec06
Alexander Crompton <alexcromp@gmail.com>
2020-08-14 13:04:32 +0200

    Clean query and convert to vec

--- src/calculator/web.clj
+++ src/calculator/web.clj
@@ -3,13 +3,31 @@
             [ring.adapter.jetty :as jetty]
             [ring.middleware.json :refer [wrap-json-response]]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.edn :as edn]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [calculator.utils :as utils]))
+
+(def ops {"/" :div
+          "*" :mult
+          "+" :add
+          "-" :subt})
+
+(defn clean-query [encoded-query]
+  "TODO: error handling"
+  (-> encoded-query
+      (utils/decode-64)
+      (#(str "[" % "]"))
+      (clojure.string/replace #"[*/+-]" #(str " " (get ops %) " "))
+      (clojure.string/replace #"\(|\)" {"(" "[" ")" "]"})
+      edn/read-string))
+
+(defn go [] (clean-query "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk"))
 
 (defroutes handler
            (GET "/calculus" [query]
                 {:status 200
                  :headers {"Content-Type" "text/plain"}
-                 :body {:result (str query)
+                 :body {:result (clean-query query)
                         :error false}})
            (ANY "*" [] {:status 500
                         :headers {"Content-Type" "text/plain"}
