c52aa54be0035a3181677abc78eee157f3146770
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 21:22:06 +0200

    re-factor a GET-test

--- test/calculator/web_test.clj
+++ test/calculator/web_test.clj
@@ -2,7 +2,18 @@
   (:require [clojure.test :refer [deftest is testing]]
             [calculator.web :refer [app]]
             [cheshire.core :as cheshire]
-            [ring.mock.request :as mock]))
+            [ring.mock.request :as mock]
+            [calculator.utils :refer [encode-64 decode-64]]
+            [clojure.math.numeric-tower :as math]))
+(defn GET-test [{:keys [equation result testing-str]}]
+  (testing testing-str
+    (let [endpoint (str "/calculus?query=" (encode-64 equation))
+          response (app (mock/request :get  endpoint))
+          body (-> response :body parse-json)
+          content-type (get (:headers response) "Content-Type")]
+      (is (= (:status response) 200))
+      (is (= content-type "application/json"))
+      (is (approx= (:result body) result)))))
 
 (deftest equations-test
   (let [response (app (mock/request :get  "/calculus?query=KDQgKyA0IC0gMjAwIC8gMik="))
