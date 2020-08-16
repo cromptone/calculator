f2d4eb89c73b3e0274938abaa7126b1523781794
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 21:23:39 +0200

    Add sample query data

--- test/calculator/web_test.clj
+++ test/calculator/web_test.clj
@@ -12,6 +12,23 @@
 
 (defn parse-json [body]
   (cheshire/parse-string body true))
+
+(def sample-query-data
+  [{:equation "- (-2 - -2) * 10 / 2 " :result 0 :testing-str "with negation"}
+   {:equation ".5 + .5 + .5" :result 1.5 :testing-str "with a decimal without zero in front"}
+   {:equation ".5+.5+.5+(.43-(1.2+1*.34))" :result 0.39 :testing-str "with a decimal without zero in front"}
+   {:equation "2 / 4 + -3 * 4 - -10.2" :result -1.3 :testing-str "with order of operations"}
+   {:equation "2 + (2) + (2 + 2 + (2 + 100 * 200 + 20 / (400/100 + 1)))" :result 20014 :testing-str "with parens"}
+   {:equation "((1/999.0)*(1/999.0)*(1/999.0)*(1/999.0)*(1/999.0)*(1/999.0)*(1/999.0))*100000000000000000000000"
+    :result 100.702808421 :testing-str "with long decimals"}
+   {:equation "3/9" :result (/ 3 9) :testing-str "with repeating rationals"}
+   {:equation "-       2 + 3 --4 *  8 -(4)  " :result 29 :testing-str "with odd whitespace"}
+   {:equation "-3" :result -3 :testing-str "with single negative number"}
+   {:equation "1" :result 1 :testing-str "with single number"}
+   {:equation "[2 +(3 +[4 *[7+8]+[9+1.1]*4])*4 /3 ]" :result 139.86666667 :testing-str "with [] and ()"}
+   {:equation "[3 + [2 + (1 +3)+3]]" :result 12 :testing-str "with [] and ()"}
+   {:equation "(((1 + 2 - (2 + 3))))" :result -2 :testing-str "with multiple outer parens"}])
+
 (defn GET-test [{:keys [equation result testing-str]}]
   (testing testing-str
     (let [endpoint (str "/calculus?query=" (encode-64 equation))
