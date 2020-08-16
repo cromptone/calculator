784ca5bea4c68faff10d0f2bf35e4c7e38fe2e87
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 21:17:00 +0200

    Add encode-decode testing

--- test/calculator/utils_test.clj
+++ test/calculator/utils_test.clj
@@ -1,6 +1,9 @@
 (ns calculator.utils-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [calculator.utils :refer [decode-64]]))
+  (:require [calculator.utils :refer [encode-64 decode-64]]
+            [clojure.test :refer [deftest is are testing]]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]))
 
 (deftest utils
   (testing "Decoder works"
@@ -18,3 +21,8 @@
     (is (= (encode-64 "900 + 1+2+1*(3/4*6)- 40000")
            "OTAwICsgMSsyKzEqKDMvNCo2KS0gNDAwMDA="))))
 
+(defspec encode-decode 50
+         (prop/for-all [string gen/string]
+                       (= string
+                          (-> string encode-64 decode-64)
+                          (-> string encode-64 decode-64 encode-64 decode-64))))
