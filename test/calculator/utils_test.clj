18cf4055e8e0ade055682f024ad18b7435c34b4a
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 21:12:14 +0200

    Test encoder

--- test/calculator/utils_test.clj
+++ test/calculator/utils_test.clj
@@ -12,3 +12,9 @@
     (testing "without = filler"
       (is (= (decode-64 "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk")
              "2 * (23/(3*3))- 23 * (2*3)"))))
+  (testing "Encoder works"
+    (is (= (encode-64 "2 * (23/(3*3))- 23 * (2*3)")
+           "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk="))
+    (is (= (encode-64 "900 + 1+2+1*(3/4*6)- 40000")
+           "OTAwICsgMSsyKzEqKDMvNCo2KS0gNDAwMDA="))))
+
