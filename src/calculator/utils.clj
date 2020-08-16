33f79933e1335127a00f3c7989897fb6a02e0b92
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 23:54:28 +0200

    Remove try catch from encoder

--- src/calculator/utils.clj
+++ src/calculator/utils.clj
@@ -2,11 +2,9 @@
   (:import java.util.Base64))
 
 (defn decode-64 [encoded-str]
-  (try
-    (->> encoded-str
-         (.decode (Base64/getDecoder))
-         String.)
-    (catch Exception e "Error while decoding query")))
+  (->> encoded-str
+       (.decode (Base64/getDecoder))
+       String.))
 
 (defn encode-64 [unencoded-str]
   (.encodeToString (Base64/getEncoder) (.getBytes unencoded-str)))
