2f0ec7cfd009948788a97ff1a2bd9941d0a36dcb
Alexander Crompton <alexcromp@gmail.com>
2020-08-16 21:12:04 +0200

    Add testing for without = base64 filler

--- test/calculator/utils_test.clj
+++ test/calculator/utils_test.clj
@@ -4,7 +4,11 @@
 
 (deftest utils
   (testing "Decoder works"
-    (is (decode-64 "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk")
-        "2 * (23/(3*3))- 23 * (2*3)")
-    (is (decode-64 "OTAwICsgMSsyKzEqKDMvNCo2KS0gNDAwMDA")
-        "900 + 1+2+1*(3/4*6)- 40000")))
+    (testing "with = filler"
+      (is (= (decode-64 "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk=")
+             "2 * (23/(3*3))- 23 * (2*3)"))
+      (is (= (decode-64 "OTAwICsgMSsyKzEqKDMvNCo2KS0gNDAwMDA=")
+             "900 + 1+2+1*(3/4*6)- 40000")))
+    (testing "without = filler"
+      (is (= (decode-64 "MiAqICgyMy8oMyozKSktIDIzICogKDIqMyk")
+             "2 * (23/(3*3))- 23 * (2*3)"))))
